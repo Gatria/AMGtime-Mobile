c55aa8f66ae59bf5040920db5af3d5f27c37347e
Gatria <neyronix@sbcglobal.net>
2020-05-30 01:57:48 -0700

    2 files modified, 1 added

--- src/app/punchdialog/punchdialog.component.ts
+++ src/app/punchdialog/punchdialog.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+ 
 
 @Component({
   selector: 'app-punchdialog',
@@ -7,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PunchdialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef,
+    @Inject(MAT_DIALOG_DATA) public data) { }
 
   ngOnInit() {
   }
