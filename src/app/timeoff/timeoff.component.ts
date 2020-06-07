3780279a807511cb2f28581b828626d982242536
Gatria <neyronix@sbcglobal.net>
2020-06-07 00:01:00 -0700

    5 files modified

--- src/app/timeoff/timeoff.component.ts
+++ src/app/timeoff/timeoff.component.ts
@@ -24,6 +24,9 @@ scrollto(a)
 {
 
 setTimeout(()=>{document.getElementById("slider").scrollLeft=document.getElementById("slider").parentElement.clientWidth*a},100);  
+
+  if (a==1)  this.bksvc.sendcommand((f)=>{ this.bksvc.Categories=f},"GetCategoriesAndUsers");
+
 }
 
 
