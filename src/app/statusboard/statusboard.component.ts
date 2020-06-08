2096ca0eb2dcc8874309de735527a3c10f168cf0
Gatria <neyronix@sbcglobal.net>
2020-06-07 18:05:18 -0700

    3 files modified

--- src/app/statusboard/statusboard.component.ts
+++ src/app/statusboard/statusboard.component.ts
@@ -12,47 +12,74 @@ export class StatusboardComponent implements OnInit {
 
   constructor(private bksvc:BackendService) { }
 
-
+ngOnDestroy() {
+  if (this.intervalid) {
+    clearInterval(this.intervalid);
+  }
+}
   ngOnInit() {
     this.filter=31;
-this.onResize();
+
+  
+
+  this.colorScheme = {
+    domain: ['#0f7da2', '#C7B42C', '#c62828 ', '#AAAAAA','#008266']
+   };
+this.intervalid=setInterval(()=>{
+let tt=this.bksvc.encript("true");
+this.bksvc.sendcommand((f)=>{this.bksvc.StatusBoard=f;this.recalculate()},"GetStatusBoard","_in="+tt+"&_out="+tt+"&_lunch="+tt+"&_break="+tt+"&_absent="+tt+"&time="+this.bksvc.datetime())},10000 ) 
+  }
+recalculate()
+{
+
+
+
+var work=0
+var out=0
+var abs=0
+var lunch=0
+var br=0
+
+bksvc.StatusBoard.forEach((e)=>{
+  if (e.Status=0 ) work++;
+   if (e.Status=3 ) out++;
+    if (e.Status=4 ) abs++;
+     if (e.Status=1 ) lunch++;
+      if (e.Status=2 ) br++;
+})
+
+.Status==1)|| (filter % 32 >= 16 && item.Status==2))">
+
+
 this.single=[
   {
     "name": "Work",
-    "value": 40632
+    "value": work
    
   },
   {
     "name": "Out",
-    "value": 50000
+    "value": out
    
   },
   {
     "name": "Abscent",
-    "value": 36745
+    "value": abs
    
   },
   {
     "name": "Lunch",
-    "value": 36240
+    "value": lunch
    
   },
   {
     "name": "Break",
-    "value": 33000
+    "value": br
    
   }
   
 ]
-  
-
-  this.colorScheme = {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
-   };
-
-let tt=this.bksvc.encript("true");
-this.bksvc.sendcommand((f)=>{this.bksvc.StatusBoard=f},"GetStatusBoard","_in="+tt+"&_out="+tt+"&_lunch="+tt+"&_break="+tt+"&_absent="+tt+"&time="+this.bksvc.datetime())  
-  }
+}  
 filterme(a,i) {
   //console.log(a,i);
   return a.toLowerCase().includes(i.toLowerCase());
