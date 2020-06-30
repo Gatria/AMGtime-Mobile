d43c92760b7d9c0e24cf9640ca3b2fbbd95987c0
Gatria <neyronix@sbcglobal.net>
2020-06-29 19:38:04 -0700

    2 files modified

--- src/app/openshift/openshift.component.ts
+++ src/app/openshift/openshift.component.ts
@@ -7,10 +7,16 @@ import {BackendService} from '../backend.service';
   styleUrls: ['./openshift.component.css']
 })
 export class OpenshiftComponent implements OnInit {
-
   constructor(private bksvc:BackendService) { }
-
+viewmode=0
   ngOnInit() {
+    this.mydate=new Date();
+    this.godate() 
   }
 
+godate(a=0)
+{
+ if (a!==0) this.mydate=a;
+this.bksvc.sendcommand((f)=>{this.bksvc.AdvancedScheduling=f;},"GetAdvancedScheduling",{date:this.mydate.f1(),viewMode:this.viewmode}) 
+}
 }
--- src/app/shiftrequest/shiftrequest.component.ts
+++ src/app/shiftrequest/shiftrequest.component.ts
@@ -8,16 +8,10 @@ import {BackendService} from '../backend.service';
 })
 export class ShiftrequestComponent implements OnInit {
 
+
   constructor(private bksvc:BackendService) { }
-viewmode=0
+
   ngOnInit() {
-    this.mydate=new Date();
-    this.godate() 
   }
 
-godate(a=0)
-{
- if (a!==0) this.mydate=a;
-this.bksvc.sendcommand((f)=>{this.bksvc.AdvancedScheduling=f;},"GetAdvancedScheduling",{date:this.mydate.f1(),viewMode:this.viewmode}) 
-}
 }
