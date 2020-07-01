5629960f9d6be9fd84705f0d95f3b009202c14c9
Gatria <neyronix@sbcglobal.net>
2020-07-01 15:50:03 -0700

    2 files modified

--- src/app/homescreen/homescreen.component.ts
+++ src/app/homescreen/homescreen.component.ts
@@ -1,27 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import {BackendService} from '../backend.service'
 import { ChooseemployeeComponent } from '../chooseemployee/chooseemployee.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-homescreen',
   templateUrl: './homescreen.component.html',
-  styleUrls: ['./homescreen.component.css']
+styleUrls: ['../statusboard/statusboard.component.scss']
 })
 export class HomescreenComponent implements OnInit {
 Sections=["Today's","Employee Current Statuses","New"];
 
 
   constructor(private bksvc:BackendService) { }
-
+ colorScheme={domain:['#0f7da2','#C7B42C','#c62828','#AAAAAA','#008266']}
   ngOnInit() { 
-    if (!this.bksvc.AMGSettings.IsEmployee) this.GetSummaryInfo()
+    if (!this.bksvc.AMGSettings.IsEmployee)  this.GetSummaryInfo()
 
   }
 GetSummaryInfo() {
 
 
 this.bksvc.sendcommand((f)=>{this.bksvc.SummeryInfo=f;
-console.log(this.bksvc.SummeryInfo);
+this.colorScheme.domain=[];
+this.single=[]
+if (this.work>0) {this.colorScheme.domain.push('#0f7da2'); this.single.push({"name": this.bksvc.SummeryInfo[2].Name,"value": this.bksvc.SummeryInfo[2].Count});}
+if (this.out>0) {this.colorScheme.domain.push('#049275'); this.single.push({"name": this.bksvc.SummeryInfo[3].Name,"value": this.bksvc.SummeryInfo[3].Count});}
+if (this.abs>0) {this.colorScheme.domain.push('#c62828'); this.single.push({"name": this.bksvc.SummeryInfo[4].Name,"value": this.bksvc.SummeryInfo[4].Count});}
 
 },"GetSummaryInfo",{time:new Date().f1()})  
 
