401eb254d2878fc0b5a0a8fba1c0f630d9a49b2d
Gatria <neyronix@sbcglobal.net>
2020-07-05 13:44:04 -0700

    1 file modified

--- src/app/attendance/attendance.component.ts
+++ src/app/attendance/attendance.component.ts
@@ -25,7 +25,7 @@ scroll() {
 getperiod(p)
 {
  this.period+=p; 
-this.bksvc.sendcommand((f)=>{this.bksvc.timecard=f} ,"GetTimeCard",{id:this.myEmployee.Id,period:this.period});
+this.bksvc.sendcommand((f)=>{this.bksvc.timecard=f;  this.bksvc.scroll()} ,"GetTimeCard",{id:this.myEmployee.Id,period:this.period});
 
 
 }
