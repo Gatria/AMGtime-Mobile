a201dead9afbcf4ea768966d3fe8d232d9d84bf7
Gatria <neyronix@sbcglobal.net>
2020-07-07 20:58:09 -0700

    1 file modified

--- src/app/usigned/usigned.component.ts
+++ src/app/usigned/usigned.component.ts
@@ -22,8 +22,8 @@ this.bksvc.sendcommand((f)=>{this.Document=f;},"*GetDocuments")
 
 scrollto(a)
 {
-
-  document.getElementById("slider").scrollLeft=document.getElementById("slider").parentElement.clientWidth*a
+setTimeout(()=>{
+  document.getElementById("slider").scrollLeft=document.getElementById("slider").parentElement.clientWidth*a},100);  
 }
 
 scroll() {
